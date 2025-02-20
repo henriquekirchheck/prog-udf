c3b972347fd88b474f0d1c4741b1a1adf856d9ef
Henrique Kirch Heck <86362827+henriquekirchheck@users.noreply.github.com>
2025-02-19 21:47:41 -0300

    aula 1

--- main.py
+++ main.py
@@ -1,5 +1,55 @@
+import requests
+from dataclasses import dataclass
+from dacite import from_dict
+
+
+@dataclass
+class LocationRef:
+    name: str
+    url: str
+
+
+@dataclass
+class CharacterData:
+    id: int
+    name: str
+    status: str
+    species: str
+    type: str
+    gender: str
+    image: str
+    episode: list[str]
+    origin: LocationRef
+    location: LocationRef
+
+
+@dataclass
+class LocationData:
+    id: int
+    name: str
+    type: str
+    dimension: str
+    residents: list[str]
+    url: str
+
+
+@dataclass
+class EpisodeData:
+    id: int
+    name: str
+    air_date: str
+    episode: str
+    characters: list[str]
+    url: str
+
+
 def main():
-    print("Hello from prog-udf!")
+    r = requests.get("https://rickandmortyapi.com/api/character/214")
+    character = from_dict(CharacterData, r.json())
+    print(f"Nome: {character.name}")
+    print(f"Status: {character.status}")
+    print(f"Genero: {character.gender}")
+    print(f"Especie: {character.species}")
 
 
 if __name__ == "__main__":
